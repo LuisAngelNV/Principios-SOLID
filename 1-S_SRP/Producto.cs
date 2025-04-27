d6c4cc74eb36d1fe2a10b4ab13f3375da009fa4e
LuisAngelNV <deanda.1998@gmail.com>
2025-04-26 18:23:51 -0600

    Add project files.

---
+++ 1-S_SRP/Producto.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrinipiosSOLID._1_S_SRP
+{
+    public class Producto
+    {
+        public string Nombre { get; set; }
+        public decimal Precio { get; set; }
+    }
+
+    public class  CalculoDeImpuestos
+    {
+        public decimal CalcularImpuesto(Producto producto)
+        {
+            Console.WriteLine("Calculando impuestos...");
+            return producto.Precio* 0.16m;
+        }
+    }
+
+    public class  ImpresionDeFacturas
+    {
+        public void ImprimirFactura(Producto producto)
+        {
+            Console.WriteLine("Imprimiendo factura...");
+            Console.WriteLine($"Producto: {producto.Nombre}, Precio: {producto.Precio}");
+        }
+    }
+
+    public class GuardarFacturas
+    {
+        public void GuardarFactura(Producto producto)
+        {
+            Console.WriteLine($"Guardando factura de {producto.Nombre} en la base de datos...");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using PrinipiosSOLID._1_S_SRP;
+
+Producto producto = new Producto { Nombre = "Laptop", Precio = 15000m };
+
+CalculoDeImpuestos calculadora = new CalculoDeImpuestos();
+decimal impuesto = calculadora.CalcularImpuesto(producto);
+Console.WriteLine($"Impuesto: {impuesto}");
+
+ImpresionDeFacturas impresora = new ImpresionDeFacturas();
+impresora.ImprimirFactura(producto);
+
+GuardarFacturas guardador = new GuardarFacturas();
+guardador.GuardarFactura(producto);
